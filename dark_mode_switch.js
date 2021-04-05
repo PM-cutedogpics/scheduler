90d1b6f228f0c9eb0bc32682f2297018beb92073
PM-cutedogpics <pete_choi@dlsu.edu.ph>
2021-04-05 18:43:09 +0800

    create schedule

--- dark_mode_switch.js
+++ dark_mode_switch.js
@@ -10,13 +10,7 @@ window.addEventListener("load", function () {
 
 /**
  * Summary: function that adds or removes the attribute 'data-theme' depending if
- * the switch is 'on' or 'off'.
- *
- * Description: initTheme is a function that uses localStorage from JavaScript DOM,
- * to store the value of the HTML switch. If the switch was already switched to
- * 'on' it will set an HTML attribute to the body named: 'data-theme' to a 'dark'
- * value. If it is the first time opening the page, or if the switch was off the
- * 'data-theme' attribute will not be set.
+ * the switch is toggled or not.
  * @return {void}
  */
 function initTheme() {
@@ -30,8 +24,8 @@ function initTheme() {
 }
 
 /**
- * Summary: resetTheme checks if the switch is 'on' or 'off' and if it is toggled
- * on it will set the HTML attribute 'data-theme' to dark so the dark-theme CSS is
+ * Summary: resetTheme checks if the switch is toggled or not and if it is toggled
+ * it will set the HTML attribute 'data-theme' to dark so the dark-theme CSS is
  * applied.
  * @return {void}
  */
