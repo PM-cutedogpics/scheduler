be7f82c54b4d0fd6ee442fdffb040714dea5adc7
PM-cutedogpics <pete_choi@dlsu.edu.ph>
2021-04-29 15:20:44 +0800

    fixed conflict routes
    conflict in home_in.html

--- routes/routes.js
+++ routes/routes.js
@@ -64,4 +64,21 @@ app.get('/view_account', function (req, res){
 	res.render('view_account');
 })
 
+app.get("/home", (req, res) => {
+	var posts = [];
+	for (var i = 0; i < 5; i++) {
+		var post = {
+			schedcard: "schedcard-" + (i + 1),
+			postImg: "/img/example" + (i + 1) + ".jpg",
+		};
+		posts.push(post);
+		console.log(posts[i].schedcard);
+		console.log(posts[i].postImg);
+	}
+	console.log(posts.length);
+	res.render("home", posts);
+	console.log(posts.length);
+});
+
+
 module.exports = app;
